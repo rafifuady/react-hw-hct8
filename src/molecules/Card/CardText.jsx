264c70adcc2ffbe2255f43d01f2b97c7069c174f
Rafi-PC <rafifuady@gmail.com>
2020-03-07 22:25:49 +0700

    Refactor component CardText

--- src/molecules/Card/CardText.jsx
+++ src/molecules/Card/CardText.jsx
@@ -1,23 +1,18 @@
 import React from 'react';
 import Content from "../../atom/Content";
 
-let CardText = () => {
+let CardText = (props) => {
     return (
         <React.Fragment>
             <div className="card">
               <div className="card-body">
-                <Content className="h5 card-title" value="Hacktiv8 Meetup" />
-                <Content className="card-text" value="Hello, Havascript and Node.js Ninjas!" />
-                <Content className="card-text" value="Get Ready for our monthly meetup JakartaJS! This is will be our fifth meetup of 2018! 
-                  The meet up format will contain some short stories and technical talks. 
-                  If you have a short announcement you'd like to share with the audience, 
-                  you may do so during open mic announcement" />
-                  ----
-                  <br></br>
-                  See You there
-                  <br></br>
-                  Best, Hengki, Giovanni, Sofian, Riza, 
-                  Agung The JakartaJS Organizers
+                <Content className="h5 card-title" value={props.valTitle} />
+                <Content className="card-text" value={props.valGreet} />
+                <Content className="card-text" value={props.valBody} />
+                -----
+                <Content className="" value={props.valFooter} />
+                <Content className="" value={props.valSign} />
+                 
 
               </div>
             </div>

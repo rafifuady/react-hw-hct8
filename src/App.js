3b5d96413cb9ee6848cf1c1d32f1b16f2c8b6a06
Rafi-PC <rafifuady@gmail.com>
2020-03-07 22:26:37 +0700

    more refactor on Apps

--- src/App.js
+++ src/App.js
@@ -13,42 +13,87 @@ class App extends React.Component {
         <NavigationBar />
         <div className="container mx-1">
           <div className="container my-3">
-            <Card  /> 
+            <Card  
+              srcImg="https://dummyimage.com/lrgrec"
+              classImg="img-fluid"
+              valTitle="Hacktiv8 Meetup"
+              valSubtitle="Jakarta, Indonesia"
+              valBody="Member 1078"
+              valBtn="Join Us"
+              hrefBtn="http://www.google.com"
+              classBtn="btn btn-primary"  
+            /> 
           </div>
 
           <Content className="h5" value ="NEXT MEETUP" />
           
           <div className="container my-3">
-            <CardText /> 
+            <CardText
+              valTitle="Hacktiv8 Meetup"
+              valGreet="Hello, Havascript and Node.js Ninjas!"
+              valBody="Get Ready for our monthly meetup JakartaJS! This is will be our fifth meetup of 2018! 
+                  The meet up format will contain some short stories and technical talks. 
+                  If you have a short announcement you'd like to share with the audience, 
+                  you may do so during open mic announcement"
+              valFooter="See You there"
+              valSign="Best, Hengki, Giovanni, Sofian, Riza, 
+              Agung The JakartaJS Organizers"
+              /> 
           </div>
 
           <Content className="h5" value ="About Meetup" />
           <Content className="p container" value="Come and meet other developers interested in the JavaScript and it's library in the Greater Jakarta area" />
           <Content className="p container" value="Twitter: JakartaJS and we use the hashtag #jakartajs" />
           
-          <h5 class="">Members</h5>
-          <div className="container my-3">
-            <div className="card">
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-2">
-                    <img src="https://www.theramblehotel.com/wp-content/uploads/2017/11/dummy-800x800.jpg" className="img-fluid img-thumbnail rounded-circle" alt="img"></img>
-                  </div>
-                  <div className="col-6">
-                    <h5 className="card-title">Organizers</h5>
-                    <br></br>
-                    <p className="card-text">Adhy Wiranata,     4 Others</p>
-                  </div>
-                </div> 
-              </div>
-              </div> 
-            </div>
+          <Content className="h5" value ="Members" />
 
-          <h5 class="">Past Meetup</h5>
+          <div className="container my-3">
+            <Card 
+              srcImg="https://www.theramblehotel.com/wp-content/uploads/2017/11/dummy-800x800.jpg"
+              classImg="img-fluid img-thumbnail rounded-circle"
+              valTitle="Organizers"
+              valBody="Adhy Wiranata,     4 Others"              
+            />
+            
+          </div>
+
+          <Content className="h5" value ="Past Meetup" />
           
           <div className="container">
             <div class="card-deck">
-              <div class="card">
+              <Card 
+                srcImg="https://dummyimage.com/lrgrec"
+                classImg="img-fluid"
+                valTitle="Hacktiv8 Meetup"
+                valSubtitle="Jakarta, Indonesia"
+                valBody="Member 1078"
+                valBtn="Join Us"
+                hrefBtn="http://www.google.com"
+                classBtn="btn btn-primary"
+              />
+
+              <Card 
+                srcImg="https://dummyimage.com/lrgrec"
+                classImg="img-fluid"
+                valTitle="Hacktiv8 Meetup"
+                valSubtitle="Jakarta, Indonesia"
+                valBody="Member 1078"
+                valBtn="Join Us"
+                hrefBtn="http://www.google.com"
+                classBtn="btn btn-primary"
+              />
+
+              <Card 
+                srcImg="https://dummyimage.com/lrgrec"
+                classImg="img-fluid"
+                valTitle="Hacktiv8 Meetup"
+                valSubtitle="Jakarta, Indonesia"
+                valBody="Member 1078"
+                valBtn="Join Us"
+                hrefBtn="http://www.google.com"
+                classBtn="btn btn-primary"
+              />
+              {/* <div class="card">
                 <div class="card-body">
                   <h4 class="card-title">27 November 2017</h4>
                   <p class="card-text">Text</p>
@@ -71,7 +116,7 @@ class App extends React.Component {
                   <h6 className="card-subtitle mb-2 text-muted">110 went</h6>
                   <a name="" id="" className="btn btn-primary" href="https://github.com/rafifuady" role="button">View</a>
                 </div>
-              </div>
+              </div> */}
               
             </div>
           </div>  
